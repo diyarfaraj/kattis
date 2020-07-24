4ea7383dd5384fb836c914e6c203a2a7bc73d894
Diyar Faraj <48604250+diyarfaraj@users.noreply.github.com>
2020-07-24 23:08:47 +0200

    stuck in a time loop kattis solution in java
    stuck in a time loop kattis solution in java

---
+++ Java/Stuck In A Time Loop.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int nr = input.nextInt();
+
+        for (int i = 0; i < nr; i++) {
+            System.out.println(i+1 + " Abracadabra");
+        }
+
+    }
+}
