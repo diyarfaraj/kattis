9ea8d3b5e3e363a03b768a9d693650b5df6271e1
Diyar Faraj <48604250+diyarfaraj@users.noreply.github.com>
2020-07-25 20:20:03 +0200

    Hanging Out on the Terrace kattis solution in java
    Hanging Out on the Terrace kattis solution in java

---
+++ Java/Hanging Out on the Terrace.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String[] input = scanner.nextLine().split(" ");
+
+        int L = Integer.parseInt(input[0]);
+        int x = Integer.parseInt(input[1]);
+
+        int notEnter = 0;
+        int persons = 0;
+
+        while (x-- > 0) {
+            Scanner sc = new Scanner(System.in);
+            String[] activity = sc.nextLine().split(" ");
+            var event = plusOrMinus(activity[0], Integer.parseInt(activity[1]));
+            persons += event;
+
+            if(persons > L){
+                notEnter++;
+                persons -= event;
+            }
+        }
+
+        System.out.println(notEnter);
+
+    }
+
+    public static int plusOrMinus(String str, int nr){
+        if(str.toLowerCase().equals("enter")){
+             nr = nr;
+        } else if(str.toLowerCase().equals("leave")){
+            nr = -Math.abs(nr);
+        }
+        return nr;
+    }
+}
